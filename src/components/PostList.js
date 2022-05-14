0333f5e4edb0fcba6302227857c20b6fa020f400
Mohamed Habarneh <92408806+MohamedHabarneh@users.noreply.github.com>
2022-05-14 12:28:58 -0700

    Update postlist.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,21 @@
-import React from "react";
-
+import React,{useState} from "react";
+import axios from "axios";
 export default function PostList(postList) {
+  
+  //idk if this is how to get all posts
+  // const[posts,getPosts] = useState('');
+  
+  // const url = 'http://localhost:80/api/postJob.php'
+
   const Post = (post) => {
-    console.log("hello");
-    return <div>hello</div>;
+  //   axios.get(url).then(response=>{
+  //     const allPosts = response.data.JobPostTitle;
+  //     getPosts(allPosts)
+  //   }).catch(error=> console.log(`Error: ${error}`));
+  //   console.log(posts,1)
+    return <div>
+      <p>Hello</p>
+    </div>;
   };
 
   return (
